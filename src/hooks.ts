20c531d4095a75fc0b49380bbd2917638d8b1070
Anwesh43 <anweshthecool0@gmail.com>
2021-07-08 00:17:15 +0530

    adding useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, CSSProperties} from 'react'
 
 const scGap : number = 0.02 
 const delay : number = 20 
@@ -38,12 +38,49 @@ export const useDimension = () => {
         }
         return () => {
             window.onresize = () => {
-                
+
             }
         }
     })
     return {
         w, 
         h
+    }
+}
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const barW : number = Math.min(w, h) / 10 
+    const barH : number = barW / 3
+    const sf : number = Math.sin(scale * Math.PI)
+    const position = 'absolute'
+    return {
+        parentStyle() : CSSProperties {
+            const left = `${w / 2}px`
+            const top = `${h / 2}px`
+            return {
+                position, 
+                left, 
+                top 
+            }
+        }, 
+        barStyle(i : number) {
+            const ji : number = Math.floor((i / 2))
+            const ai : number = Math.floor((i + 1) / 2)
+            const left = `${-barW / 2 + barW * ai}px`
+            const top = `${-barH / 2 + h * .5 * (1 - sf)}px`
+            const transform = `rotate(${180 * ji}deg)`
+            const width = `${-barW}px`
+            const height = `${-barH}px`
+            const background = 'indigo'
+            return {
+                position, 
+                left, 
+                top, 
+                transform, 
+                width, 
+                height,
+                background
+            }
+        }
     }
 }
